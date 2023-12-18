77272046a08f70f2a69a1db6d6598f2f0b1ebe25
David Hurrell <dhurrell@hotmail.co.uk>
2023-12-18 17:55:05 +0000

    add day labels

--- MonthlyPlannerGUIV2.py
+++ MonthlyPlannerGUIV2.py
@@ -1,31 +1,38 @@
 import tkinter as tk
 
 
-def create_entry(parent, row, column):
-    return tk.Entry(parent, width=5, borderwidth=0.5, relief="solid").grid(row=row, column=column,  sticky="nsew")
+# def create_entry(parent, row, column):
+#     return tk.Entry(parent, width=5, borderwidth=0.5, relief="solid").grid(row=row, column=column,  sticky="nsew")
 
 
 # Create the main window
 root = tk.Tk()
-root.title("Tkinter Grid with Border")
+root.title("Monthly Planner")
 root.config(bg="lightblue")
 
 
 # Set the size of the window to cover most of the screen
 screen_width, screen_height = root.winfo_screenwidth(), root.winfo_screenheight()
 window_width, window_height = int(
-    screen_width * 0.85), int(screen_height * 0.9)
+    screen_width * 0.85), int(screen_height * 0.95)
 root.geometry(
     f"{window_width}x{window_height}+{int((screen_width - window_width) / 2)}+{int((screen_height - window_height) / 2)}")
 labelList = []
 calenderDays = [27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
                 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 1, 2, 3, 4, 5, 6, 7, 8]
+day_labels = ['Monday', 'Tuesday', 'Wednesday',
+              'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 title = tk.Label(text="PLANNER",
-                 background='#EEEEEE', font=('ariel', 12), width=17,  border=1, bg='lightblue')
+                 background='#EEEEEE', font=('ariel', 12), width=17, pady=5, bg='lightblue')
 title.grid(column=2, row=0, columnspan=3)
+
+for num in range(7):
+    dayLabel = tk.Label(text=day_labels[num],
+                        background='#EEEEEE', font=('ariel', 12), width=17,  border=1, bg='lightblue')
+    dayLabel.grid(column=num, row=1)
 # create row and use modulus to create alternative label and text box rows
-for rows in range(2, 14):
+for rows in range(4, 16):
 
     if rows % 2 == 0:
         for cols in range(0, 7):
@@ -38,7 +45,7 @@ for rows in range(2, 14):
     else:
         for cols in range(0, 7):
             foo = tk.Text(
-                background='#EEEEEE', font=('ariel', 11), width=19, height=4, border=1,)
+                background='#EEEEEE', font=('ariel', 11), width=19, height=3.8, border=1,)
             foo.grid(column=cols, row=rows,
                      padx=5, pady=5)
             # labelList.append(foo)
