da0ddf92b26c69627385a1f53d7a7a962bd60594
David Hurrell <dhurrell@hotmail.co.uk>
2023-12-26 17:08:19 +0000

    #

--- MonthCalenderV2.py
+++ MonthCalenderV2.py
@@ -4,126 +4,126 @@ from dateutil.relativedelta import relativedelta
 import calendar
 
 # Get the current date and time
-current_datetime = datetime.now()
 
 
-def get_first_day_of_selected_month(selected_date):
-    # Get the first day of the selected month
-    return datetime(selected_date.year, selected_date.month, 1).weekday()
+class MonthPlannerDate:
+
+    def __init__(self) -> None:
+        # create a datetime objct for the date i want to be the current month. The previous and next month
+        # will be realtive to this
+        self.selectedMonth = datetime.now()
+        # Get the number of days in the selected month
+        self.days_in_selected_month = calendar.monthrange(
+            self.selectedMonth.year, self.selectedMonth.month)[1]
+        # get the first day of the selected month. This will be an integer to determine where the current month starts
+        # in matrix i.e Tuedays will return a 1. this means that the current month will start at index 1 in matrix
+        # if it was thursday it would return a 3 so it would start at index 3
+        self.currentDayAsInteger = self.get_first_day_of_selected_month(
+            self.selectedMonth)
+
+    def ChangeMonth(self, newMonth):
+        self.selectedMonth = newMonth
+        self.days_in_selected_month = calendar.monthrange(
+            self.selectedMonth.year, self.selectedMonth.month)[1]
+        self.currentDayAsInteger = self.get_first_day_of_selected_month(
+            self.selectedMonth)
+
+    def get_first_day_of_selected_month(self, selected_date):
+        # Get the first day of the selected month
+        return datetime(selected_date.year, selected_date.month, 1).weekday()
+
+    def CurrentMonth(self, selectedMonth=None):
+        if selectedMonth == None:
+            # Get the current date and time
+            current_datetime = datetime.now()
+        else:
+            return self.get_first_day_of_selected_month(selectedMonth)
+        # Get the first day of the current month
+        first_day_of_current_month = datetime(
+            current_datetime.year, current_datetime.month, 1)
+        # Extract the day of the week as an integer (Monday is 0, Sunday is 6)
+
+        first_day_of_month_integer = first_day_of_current_month.weekday()
+
+        return first_day_of_month_integer
+
+    def PreviousMonth(self, selected_date, AmountOfPreviouMonths):
+        # Calculate the first day of 1 months ago. Takes selected_date datetime object and subracts a datetime object
+        # fron N months ago, return a new datatime onject
+        first_day_of_previous_months = selected_date - \
+            relativedelta(months=AmountOfPreviouMonths, day=1)
+        # Get the number of days in previous month.Monthrange returns a tuple (day of week month starts, amount of days in month)
+        # returns second element of object [1]
+        days_in_month = calendar.monthrange(
+            first_day_of_previous_months.year, first_day_of_previous_months.month)[1]
+
+        return days_in_month
+
+    def FillMatrixCurrentMonth(self, dayOfMonthInteger, days_in_current_month, calenderDays):
+        # Start to fill the calender from the first day of the month.
+        # using day datetime integer as a start index 0-Monday 1- Tuesday etc
+        # Get the number of days in the current month
+        # set the first day as 1 in the calender display
+        beginning_new_month_output = 1
+
+        for thisMonth in range(dayOfMonthInteger, days_in_current_month + dayOfMonthInteger):
+            calenderDays.append(beginning_new_month_output)
+            beginning_new_month_output += 1
+
+    def FillMatrixPreviousMonth(self, currentDayAsInteger, selected_date, AmountOfPreviouMonths, calenderDays):
+        # fill the calender days before the current month start date with
+        # last days of previous month.i.e (28 29 30) 1 2 3
+        previousMonthNumberOfDays = self.PreviousMonth(
+            selected_date, AmountOfPreviouMonths)
+        last_month_day_start = previousMonthNumberOfDays - currentDayAsInteger + 1
+    # Start to fill calender matrix with previous months days if thi month doe not start on a Monday
+        for day in range(0, currentDayAsInteger):
+            calenderDays.append(last_month_day_start)
+            last_month_day_start += 1
+
+    def FillMatrixNextMonth(self, dayOfMonthInteger, selected_date, calenderDays):
+        # fill matrix after current month end date with first days
+        # of the next month i.e 28 29 30 (1 2 3)
+        beginning_next_month_output = 1
+        days_in_current_month = calendar.monthrange(
+            selected_date.year, selected_date.month)[1]
+        # fill any remaining slot in matrix with next months day starting at one.
+        for day in range(days_in_current_month+dayOfMonthInteger, 42):
+            # calendrDisplay[nextMonth] = beginning_next_month_output
+            calenderDays.append(beginning_next_month_output)
+            beginning_next_month_output += 1
+
+    def SendCalendarDayToGUI(self):
+        calenderDays = []
+
+        self.FillMatrixPreviousMonth(
+            self.currentDayAsInteger, self.selectedMonth, 1, calenderDays)
+
+        self.FillMatrixCurrentMonth(
+            self.currentDayAsInteger, self.days_in_selected_month, calenderDays)
+        # FillMatrixCurrentMonth(
+        #     calenderDisplay, currentDayAsInteger, days_in_selected_month,calenderDays)
+
+        self.FillMatrixNextMonth(self.currentDayAsInteger,
+                                 self.selectedMonth, calenderDays)
+        return calenderDays
+
+    def SendSelectedDateToGUI(self):
+        return self.selectedMonth
 
 
-def CurrentMonth(selectedMonth=None):
-    if selectedMonth == None:
-        # Get the current date and time
-        current_datetime = datetime.now()
-    else:
-        return get_first_day_of_selected_month(selectedMonth)
-    # Get the first day of the current month
-    first_day_of_current_month = datetime(
-        current_datetime.year, current_datetime.month, 1)
-    # Extract the day of the week as an integer (Monday is 0, Sunday is 6)
-
-    first_day_of_month_integer = first_day_of_current_month.weekday()
-
-    return first_day_of_month_integer
-
-
-def PreviousMonth(selected_date, AmountOfPreviouMonths):
-    # Calculate the first day of 1 months ago. Takes selected_date datetime object and subracts a datetime object
-    # fron N months ago, return a new datatime onject
-    first_day_of_previous_months = selected_date - \
-        relativedelta(months=AmountOfPreviouMonths, day=1)
-    # Get the number of days in previous month.Monthrange returns a tuple (day of week month starts, amount of days in month)
-    # returns second element of object [1]
-    days_in_month = calendar.monthrange(
-        first_day_of_previous_months.year, first_day_of_previous_months.month)[1]
-
-    return days_in_month
-
-
-def CreateCalendarMatrix():
-    # set a list for 42 days ( 7 days time 6 weeks)
-    calender_day_slots = [0 for x in range(0, 42)]
-
-    return calender_day_slots
-
-
-def FillMatrixCurrentMonth(dayOfMonthInteger, days_in_current_month, calenderDays):
-    # Start to fill the calender from the first day of the month.
-    # using day datetime integer as a start index 0-Monday 1- Tuesday etc
-    # Get the number of days in the current month
-    # set the first day as 1 in the calender display
-    beginning_new_month_output = 1
-
-    for thisMonth in range(dayOfMonthInteger, days_in_current_month + dayOfMonthInteger):
-
-       # calenderDisplay[thisMonth] = beginning_new_month_output
-        calenderDays.append(beginning_new_month_output)
-        beginning_new_month_output += 1
-    return calenderDisplay
-
-
-def FillMatrixPreviousMonth(currentDayAsInteger, selected_date, AmountOfPreviouMonths, calenderDays):
-    # fill the calender days before the current month start date with
-    # last days of previous month.i.e (28 29 30) 1 2 3
-    previousMonthNumberOfDays = PreviousMonth(
-        selected_date, AmountOfPreviouMonths)
-    last_month_day_start = previousMonthNumberOfDays - currentDayAsInteger + 1
-# Start to fill calender matrix with previous months days if thi month doe not start on a Monday
-    for day in range(0, currentDayAsInteger):
-
-        # [day] = last_month_day_start
-        calenderDays.append(last_month_day_start)
-        last_month_day_start += 1
-    return calenderDisplay
-
-
-def FillMatrixNextMonth(dayOfMonthInteger, selected_date, calenderDays):
-    # fill matrix after current month end date with first days
-    # of the next month i.e 28 29 30 (1 2 3)
-    beginning_next_month_output = 1
-    days_in_current_month = calendar.monthrange(
-        selected_date.year, selected_date.month)[1]
-    # fill any remaining slot in matrix with next months day starting at one.
-    for day in range(days_in_current_month+dayOfMonthInteger, 42):
-        # calendrDisplay[nextMonth] = beginning_next_month_output
-        calenderDays.append(beginning_next_month_output)
-        beginning_next_month_output += 1
-
-
-def SendCalendarDayToGUI():
-    calenderDays = []
-
-    FillMatrixPreviousMonth(
-        currentDayAsInteger, selectedMonth, 1, calenderDays)
-
-    FillMatrixCurrentMonth(
-        currentDayAsInteger, days_in_selected_month, calenderDays)
-    # FillMatrixCurrentMonth(
-    #     calenderDisplay, currentDayAsInteger, days_in_selected_month,calenderDays)
-
-    FillMatrixNextMonth(currentDayAsInteger,
-                        selectedMonth, calenderDays)
-    return calenderDays
-
-
-def SendSelectedDateToGUI():
-    return selectedMonth
-
-
-# currentDayAsInteger = CurrentMonth()
 # create a datetime objct for the date i want to be the current month. The previous and next month
 # will be realtive to this
-selectedMonth = datetime(2024, 1, 1)
-# Get the number of days in the selected month
-days_in_selected_month = calendar.monthrange(
-    selectedMonth.year, selectedMonth.month)[1]
-# get the first day of the selected month. This will be an integer to determine where the current month starts
-# in matrix 1.e Tuedays will return a 1. this means that the current month will start at index 1 in matrix
-# if it was thursday it would return a 3 so it would start at index 3
-currentDayAsInteger = get_first_day_of_selected_month(selectedMonth)
-calenderDisplay = CreateCalendarMatrix()
-calenderDays = []
+# selectedMonth = datetime(2024, 1, 1)
+# # Get the number of days in the selected month
+# days_in_selected_month = calendar.monthrange(
+#     selectedMonth.year, selectedMonth.month)[1]
+# # get the first day of the selected month. This will be an integer to determine where the current month starts
+# # in matrix 1.e Tuedays will return a 1. this means that the current month will start at index 1 in matrix
+# # if it was thursday it would return a 3 so it would start at index 3
+# currentDayAsInteger = get_first_day_of_selected_month(selectedMonth)
+
+# calenderDays = []
 
 # FillMatrixPreviousMonth(
 #     calenderDisplay, currentDayAsInteger, selectedMonth, 1, calenderDays)
--- MonthlyPlannerGUIV2.py
+++ MonthlyPlannerGUIV2.py
@@ -1,70 +1,102 @@
+
 import tkinter as tk
-from MonthCalenderV2 import SendCalendarDayToGUI, SendSelectedDateToGUI
+from MonthCalenderV2 import MonthPlannerDate
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+sampleBoxInputs = []
+
+
+def on_text_box_focus_out(event, row, col):
+    # Get the content of the text box when the focus is out
+    content = textBoxList[(row - 4) // 2 * 7 + col].get("1.0", tk.END)
+
+    print(f"Content of text box at row {row}, column {col}:\n{content}")
+    sampleBoxInputs.append(content)
+    print(f"input list = { sampleBoxInputs}")
 
 
 def OnPreviousMonthButtonClick():
-    pass
+    # Creates a new calenderDays list one month earlier than the program's current
+    # selected date
+    calenderDays = []
+    # Subtract one month from the current selected month
+    new_selected_month = DateObject.selectedMonth - relativedelta(months=1)
+    DateObject.ChangeMonth(new_selected_month)
+    calenderDays = DateObject.SendCalendarDayToGUI()
+
+    monthSelecterLabel.config(text=DateObject.selectedMonth.strftime("%B %Y"))
+    for x in range(0, 42):
+        labelList[x].config(text=calenderDays[x])
+
+
+def OnNextMonthButtonClick():
+    # Creates a new calenderDays list one month later than the program's current
+    # selected date
+    calenderDays = []
+    # Add one month from the current selected month
+    new_selected_month = DateObject.selectedMonth + relativedelta(months=1)
+    DateObject.ChangeMonth(new_selected_month)
+    calenderDays = DateObject.SendCalendarDayToGUI()
+
+    monthSelecterLabel.config(text=DateObject.selectedMonth.strftime("%B %Y"))
+    for x in range(0, 42):
+        labelList[x].config(text=calenderDays[x])
 
 
 # Program variables
+DateObject = MonthPlannerDate()
+selectedMonth = DateObject.selectedMonth
+calenderDays = DateObject.SendCalendarDayToGUI()
 
-# stores the current datetime object that is in use
-selectedMonth = SendSelectedDateToGUI()
-# list of previous, current and next month days
-calenderDays = SendCalendarDayToGUI()
-# Create the main window
 root = tk.Tk()
 root.title("Monthly Planner")
 root.config(bg="lightblue")
 
-# Set the size of the window to cover most of the screen
 screen_width, screen_height = root.winfo_screenwidth(), root.winfo_screenheight()
 window_width, window_height = int(
     screen_width * 0.96), int(screen_height * 0.96)
-# set the screen location centrally
 root.geometry(
     f"{window_width}x{window_height}+{int((screen_width - window_width) / 2)}+{int((screen_height - window_height) / 2)}")
-
-######################  Screen layout ################################
 
 day_labels = ['Monday', 'Tuesday', 'Wednesday',
               'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-title = tk.Label(text="PLANNER",
-                 background='#EEEEEE', font=('ariel', 12), width=17, pady=5, bg='lightblue')
+title = tk.Label(text="PLANNER", background='#EEEEEE', font=(
+    'ariel', 12), width=17, pady=5, bg='lightblue')
 title.grid(column=2, row=0, columnspan=3)
-monthSelecterLabel = tk.Label(text=selectedMonth.strftime("%B"), width=17)
-prevMonthButton = tk.Button(text="<<<", width=17, bg="lightyellow",
-                            )
-nextMonthButton = tk.Button(text=">>>", width=17, bg="lightyellow")
+monthSelecterLabel = tk.Label(text=selectedMonth.strftime("%B %Y"), width=17)
+prevMonthButton = tk.Button(
+    text="<<<", width=17, bg="lightyellow", command=OnPreviousMonthButtonClick)
+nextMonthButton = tk.Button(
+    text=">>>", width=17, bg="lightyellow", command=OnNextMonthButtonClick)
 
 monthSelecterLabel.grid(column=5, row=1)
 prevMonthButton.grid(column=4, row=1)
 nextMonthButton.grid(column=6, row=1)
 for num in range(7):
-    dayLabel = tk.Label(text=day_labels[num],
-                        background='#EEEEEE', font=('ariel', 12), width=17,  border=1, bg='lightblue')
+    dayLabel = tk.Label(text=day_labels[num], background='#EEEEEE', font=(
+        'ariel', 12), width=17, border=1, bg='lightblue')
     dayLabel.grid(column=num, row=2)
 
-# create row and use modulus to create alternative label and text box rows
 labelList = []  # stores list of label objects
+textBoxList = []
 for rows in range(4, 16):
-
     if rows % 2 == 0:
         for cols in range(0, 7):
-            foo = tk.Label(text="hello",
-                           background='lightblue', font=('ariel', 12), width=23, height=0, border=1)
-            foo.grid(column=cols, row=rows,
-                     padx=1, pady=3)
-            # send each instance of label to the list, where it can be accessed by list indexing
+            foo = tk.Label(text="hello", background='lightblue',
+                           font=('ariel', 12), width=23, height=0, border=1)
+            foo.grid(column=cols, row=rows, padx=1, pady=3)
             labelList.append(foo)
     else:
         for cols in range(0, 7):
-            foo = tk.Text(
-                background='#EEEEEE', font=('ariel', 11), width=19, height=3.8, border=0.5, relief=tk.RAISED, bd=2)
-            foo.grid(column=cols, row=rows,
-                     padx=1, pady=5)
-            # labelList.append(foo)
+            foo = tk.Text(background='#EEEEEE', font=('ariel', 11),
+                          width=19, height=3.8, border=0.5, relief=tk.RAISED, bd=2)
+            foo.grid(column=cols, row=rows, padx=1, pady=5)
+            textBoxList.append(foo)
+            # Bind the <FocusOut> event to each text box in textBoxList
+            foo.bind("<FocusOut>", lambda event, row=rows,
+                     col=cols: on_text_box_focus_out(event, row, col))
 
 
 def FillCalender():
@@ -73,6 +105,117 @@ def FillCalender():
 
 
 FillCalender()
-# Run the Tkinter event loop
 root.mainloop()
-print(len(labelList))
+
+
+# import tkinter as tk
+# # from MonthCalenderV2 import SendCalendarDayToGUI, SendSelectedDateToGUI
+# from MonthCalenderV2 import MonthPlannerDate
+# from datetime import datetime
+# from dateutil.relativedelta import relativedelta
+
+
+# def OnPreviousMonthButtonClick():
+#     # Creates a new calenderDays list one month earlier than the programs current
+#     # selected date
+#     calenderDays = []
+#     # Subtract one month from the current selected month
+#     new_selected_month = DateObject.selectedMonth - relativedelta(months=1)
+#     DateObject.ChangeMonth(new_selected_month)
+#     calenderDays = DateObject.SendCalendarDayToGUI()
+
+#     monthSelecterLabel.config(text=DateObject.selectedMonth.strftime("%B %Y"))
+#     for x in range(0, 42):
+#         labelList[x].config(text=calenderDays[x])
+
+
+# def OnNextMonthButtonClick():
+#     # Creates a new calenderDays list one month later than the programs current
+#     # selected date
+#     calenderDays = []
+#     # Subtract one month from the current selected month
+#     new_selected_month = DateObject.selectedMonth + relativedelta(months=1)
+#     DateObject.ChangeMonth(new_selected_month)
+#     calenderDays = DateObject.SendCalendarDayToGUI()
+
+#     monthSelecterLabel.config(text=DateObject.selectedMonth.strftime("%B %Y"))
+#     for x in range(0, 42):
+#         labelList[x].config(text=calenderDays[x])
+
+
+# # Program variables
+# DateObject = MonthPlannerDate()
+# # stores the current datetime object that is in use
+# selectedMonth = DateObject.selectedMonth
+# # list of previous, current and next month days
+# calenderDays = DateObject.SendCalendarDayToGUI()
+
+# # Create the main window
+# root = tk.Tk()
+# root.title("Monthly Planner")
+# root.config(bg="lightblue")
+
+# # Set the size of the window to cover most of the screen
+# screen_width, screen_height = root.winfo_screenwidth(), root.winfo_screenheight()
+# window_width, window_height = int(
+#     screen_width * 0.96), int(screen_height * 0.96)
+# # set the screen location centrally
+# root.geometry(
+#     f"{window_width}x{window_height}+{int((screen_width - window_width) / 2)}+{int((screen_height - window_height) / 2)}")
+
+# ######################  Screen layout ################################
+
+# day_labels = ['Monday', 'Tuesday', 'Wednesday',
+#               'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+# title = tk.Label(text="PLANNER",
+#                  background='#EEEEEE', font=('ariel', 12), width=17, pady=5, bg='lightblue')
+# title.grid(column=2, row=0, columnspan=3)
+# monthSelecterLabel = tk.Label(text=selectedMonth.strftime("%B %Y"), width=17)
+# prevMonthButton = tk.Button(text="<<<", width=17, bg="lightyellow", command=OnPreviousMonthButtonClick,
+#                             )
+# nextMonthButton = tk.Button(
+#     text=">>>", width=17, bg="lightyellow", command=OnNextMonthButtonClick)
+
+# monthSelecterLabel.grid(column=5, row=1)
+# prevMonthButton.grid(column=4, row=1)
+# nextMonthButton.grid(column=6, row=1)
+# for num in range(7):
+#     dayLabel = tk.Label(text=day_labels[num],
+#                         background='#EEEEEE', font=('ariel', 12), width=17,  border=1, bg='lightblue')
+#     dayLabel.grid(column=num, row=2)
+
+# # create row and use modulus to create alternative label and text box rows
+# labelList = []  # stores list of label objects
+# textBoxList = []
+# for rows in range(4, 16):
+
+#     if rows % 2 == 0:
+#         for cols in range(0, 7):
+#             foo = tk.Label(text="hello",
+#                            background='lightblue', font=('ariel', 12), width=23, height=0, border=1)
+#             foo.grid(column=cols, row=rows,
+#                      padx=1, pady=3)
+#             # send each instance of label to the list, where it can be accessed by list indexing
+#             labelList.append(foo)
+#     else:
+#         for cols in range(0, 7):
+#             foo = tk.Text(
+#                 background='#EEEEEE', font=('ariel', 11), width=19, height=3.8, border=0.5, relief=tk.RAISED, bd=2)
+#             foo.grid(column=cols, row=rows,
+#                      padx=1, pady=5)
+#             textBoxList.append(foo)
+
+
+# def FillCalender():
+#     for x in range(0, 42):
+#         labelList[x].config(text=calenderDays[x])
+
+
+# FillCalender()
+# # Run the Tkinter event loop
+# root.mainloop()
+# print(len(labelList))
+# print(calenderDays)
+# print(textBoxList)
+print(sampleBoxInputs)
